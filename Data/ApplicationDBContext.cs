56d01320a690dbff6a7540eb04c1e25c1c67ae7f
JoeTech-Studio <joevelez_1234@outlook.es>
2023-02-17 14:30:57 -0500

    Actualizacion

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using APIClientes.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIClientes.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Cliente{ get; set; }
+        public DbSet<Ciudad> Ciudad { get; set; }
+
+    }
+}
